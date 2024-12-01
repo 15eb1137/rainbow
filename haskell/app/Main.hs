234a51a8320b397d413f362894cbe5efcb15e575
iori tobeta <15eb1137@gmail.com>
2024-12-02 00:20:20 +0900

    haskell local init

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,6 +1,30 @@
 module Main (main) where
 
 import Lib
+import Web.Scotty
+import Network.HTTP.Types.Status (status200)
+import Network.Wai.Middleware.Cors
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as TL
+import Network.HTTP.Types.Method
+import qualified Data.ByteString.Char8 as BS
+import Network.HTTP.Types.Header (hContentType)
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Starting server on port 3000..."
+    scotty 3000 $ do
+        -- CORSミドルウェアの設定
+        middleware $ cors $ const $ Just simpleCorsResourcePolicy
+            { corsRequestHeaders = [hContentType]
+            , corsMethods = [methodGet, methodOptions]
+            }
+
+        -- GETエンドポイント
+        get (literal "/problem") $ do
+            status status200
+            json sampleProblem
+
+        -- ヘルスチェック用エンドポイント
+        get (literal "/health") $ do
+            text $ TL.pack "OK"  -- 明示的にText型に変換
--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -1,6 +1,42 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( sampleProblem
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Aeson (ToJSON)
+import GHC.Generics
+
+-- データ型の定義
+data Card = Card 
+    { suit :: String
+    , rank :: String
+    } deriving (Generic, Show)
+
+data Problem = Problem
+    { communityCards :: [Card]
+    , playerHands :: [([Card])]
+    , correctAnswer :: Int
+    } deriving (Generic, Show)
+
+-- JSONシリアライズのためのインスタンス
+instance ToJSON Card
+instance ToJSON Problem
+
+-- サンプル問題の定義
+sampleProblem :: Problem
+sampleProblem = Problem
+    { communityCards = 
+        [ Card "♠" "A"
+        , Card "♥" "K"
+        , Card "♦" "Q"
+        , Card "♣" "J"
+        , Card "♠" "10"
+        ]
+    , playerHands = 
+        [ [ Card "♥" "A", Card "♦" "K" ]  -- プレイヤー1の手札
+        , [ Card "♣" "A", Card "♠" "K" ]  -- プレイヤー2の手札
+        ]
+    , correctAnswer = 1  -- プレイヤー1が勝利
+    }
